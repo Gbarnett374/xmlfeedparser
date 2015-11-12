acadef620f32a7e0a1c54b8c2a9fbc03cb7c2cfc
Greg Barnett <Gbarnett374@gmail.com>
2015-11-12 08:16:48 -0500

    moved script execution to a separate file

---
+++ feedParser_controller.php
@@ -0,0 +1,29 @@
+<?php 
+
+require './feedParserClass.php';
+
+$url = '';
+$filter = "category";
+/*Index is the postion of the element you want to filter. 
+If the elements in the node are not separated by a delimiter then use 0. 
+If the specified index does not exist an error will be thrown.
+*/
+$index = 1;
+$delimiter = ">";
+$directory = "/tmp/feed"; 
+
+$params = [
+	"url"       => $url,
+	"filter"    => $filter,
+	"index"	    => $index,
+	"delimiter" => ">"
+];
+
+try{
+	$parser = new feedParser($params);
+	$parser->filterFeed();
+	$parser->write($directory);
+	
+} catch (Exception $e) {
+	echo $e->getMessage();
+}
